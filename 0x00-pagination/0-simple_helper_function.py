2ec08522dbc6620f28f0f2cf04d2df581161042a
wycky1990 <wyclifffeomondi@gmail.com>
2025-05-11 07:54:48 +0000

    0x00. Pagination

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Simple helper function"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Takes 2 integer arguments and returns a tuple of size two
+    
+    Args:
+        page (int): page number to return (pages are 1-indexed)
+        page_size (int): number of items per page
+    Return:
+        tuple(start_index, end_index)
+    """
+    start, end = 0, 0
+    for i in range(page):
+        start = end
+        end += page_size
+
+    return (start, end)
